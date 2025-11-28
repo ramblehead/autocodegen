b930eb6313ee56d24175c9d0c1c97e8a25f85109
ramblehead <v.rybynok@gmail.com>
2025-11-28 22:20:24 +0000

    Checkpoint

--- src/autocodegen/__init__.py
+++ src/autocodegen/__init__.py
@@ -1,6 +1,6 @@
 # Hey Emacs, this is -*- coding: utf-8; mode: python -*-
 
 from .config import Config
-from .expand import expand, generate
+from .expand import generate
 
-__all__ = ["Config", "expand", "generate"]
+__all__ = ["Config", "generate"]
