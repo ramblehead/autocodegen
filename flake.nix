67e36a697c7cb3b2af0992aebd67272d032276e0
ramblehead <v.rybynok@gmail.com>
2026-01-08 23:44:48 +0000

    Checkpoint

--- flake.nix
+++ flake.nix
@@ -89,8 +89,14 @@
             ]
           );
 
+        runtimeDepsWithProject =
+          {
+            autocodegen = pythonSet.autocodegen;
+          }
+          // workspace.deps.default;
+
         pythonEnv =
-          pythonSet.mkVirtualEnv "autocodegen-env" workspace.deps.default;
+          pythonSet.mkVirtualEnv "autocodegen-env" runtimeDepsWithProject;
       in {
         # Package a virtual environment as our main application.
         #
